ed87e7e01ae956102ea002d022b3556d21af04c6
RuslanGrigoryev <rus.grigoryev@yandex.ua>
2016-04-29 23:10:06 +0300

    Configured jshint grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function(grunt) {
 			}
 		},
 		jshint: {
-			files: ['Gruntfile.js'],
+			files: ['Gruntfile.js', 'src/js/**/*.js'],
 			options: {
 				globals: {
 					jQuery: true
