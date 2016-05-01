48fcf9bd1161337bd57760c768fd17001fdef597
RuslanGrigoryev <rus.grigoryev@yandex.ua>
2016-05-01 23:48:02 +0300

    Created header template

--- src/server/index.php
+++ src/server/index.php
@@ -20,42 +20,7 @@
 </head>
 <body>
 
-	<div class="header container">
-		<div class="row">
-			<div class="col-md-4">
-				
-				<?php include_once 'templates/donate.php'; ?>
-
-			</div>
-
-			<div class="col-md-4 logo">
-				
-				<?php include_once 'templates/logo.php'; ?>
-
-			</div>
-
-			<div class="col-md-4 user">
-				
-
-				<div class="row">
-				
-					<?php 
-						if ( $GLOBALS['isAuthorised'] ) {
-
-							include_once 'templates/user/info.php';
-							include_once 'templates/user/geolocation.php';
-
-						} else {
-
-							include_once 'templates/user/auth.php';
-
-						}
-					?>
-	
-				</div>
-			</div>
-		</div>
-	</div>
+	<?php include_once 'templates/header.php'; ?>
 
 	<script type="text/javascript" src="js/app.min.js"></script>
 </body>
