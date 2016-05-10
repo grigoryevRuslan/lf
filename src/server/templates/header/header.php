7ae04ac3e2d72ab889639fa0a07a3c65b78a9088
RuslanGrigoryev <rus.grigoryev@yandex.ua>
2016-05-10 13:03:29 +0300

    Added some styles for mobile viewport

--- src/server/templates/header/header.php
+++ src/server/templates/header/header.php
@@ -8,13 +8,13 @@
 
 		</div>
 
-		<div class="col-md-4 logo">
+		<div class="col-xs-6 col-md-4 logo">
 			
 			<?php include_once 'logo/logo.php';?>
 			
 		</div>
 
-		<div class="col-md-4 user">
+		<div class="col-xs-6 col-md-4 user">
 			
 			<div class="row">
 			
