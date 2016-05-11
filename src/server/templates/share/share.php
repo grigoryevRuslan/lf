75e5d1d20a62eeeca928cdc68e48bc77af2902bb
RuslanGrigoryev <rus.grigoryev@yandex.ua>
2016-05-11 22:18:01 +0300

    Fixed image uri during fb share

--- src/server/templates/share/share.php
+++ src/server/templates/share/share.php
@@ -1,7 +1,7 @@
 <?php 
 	$page_uri = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
-	$image_uri = "http://$_SERVER[HTTP_HOST]".$result[0]['image_uri'];
+	$image_uri = "http://$_SERVER[HTTP_HOST]".'/upload/'.$result[0]['image_uri'];
 ?>
 
  <?php if ( $GLOBALS['isAuthorised'] ) { ?>
@@ -12,12 +12,12 @@
 		
 		<i onclick="Share.facebook('<?php echo $page_uri ?>',
 									'Всеукраинское бюро находок',
-									'<?php echo $image_uri; ?>/upload/<?php echo $result[0]['image_uri']; ?>',
+									'<?php echo $image_uri; ?>',
 									'<?php echo $result[0]['item']; ?>')" class="share__btn share__btn_fb"></i>
 
 		<i onclick="Share.vkontakte('<?php echo $page_uri ?>',
 									'Всеукраинское бюро находок',
-									'<?php echo $image_uri; ?>/app/upload/<?php echo $result[0]['image_uri']; ?>',
+									'<?php echo $image_uri; ?>',
 									'<?php echo $result[0]['item']; ?>')" class="share__btn share__btn_vk"></i>
 
 		<i onclick="Share.twitter('<?php echo $page_uri ?>',
