77ec274485df57a6fc9ece076b15fc6babd71c53
RuslanGrigoryev <rus.grigoryev@yandex.ua>
2016-05-17 14:21:39 +0300

    Fixed footer uris

--- src/server/templates/footer/info.php
+++ src/server/templates/footer/info.php
@@ -1,9 +1,9 @@
 <div class="info">
 		
 	<ul class="info__links">
-		<li><a href="content/about.php">О проекте</a></li>
-		<li><a href="content/faq.php">Часто задаваемые вопросы</a></li>
-		<li><a href="content/privacy.php">Политика конфеденциальности</a></li>
+		<li><a href="http://<?php echo $GLOBALS['domain'] ?>/content/about.php">О проекте</a></li>
+		<li><a href="http://<?php echo $GLOBALS['domain'] ?>/content/faq.php">Часто задаваемые вопросы</a></li>
+		<li><a href="http://<?php echo $GLOBALS['domain'] ?>/content/privacy.php">Политика конфеденциальности</a></li>
 	</ul>
 
 	<p class="info__copyright">
