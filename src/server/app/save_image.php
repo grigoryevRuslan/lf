1cdea7698736b0e713ebb10d75f98ae78821b766
RuslanGrigoryev <rus.grigoryev@yandex.ua>
2016-05-21 02:09:17 +0300

    Fixed bug with image upload

--- src/server/app/save_image.php
+++ src/server/app/save_image.php
@@ -1,27 +1,27 @@
 <?php
-
 	if (isset($_FILES["fileToUpload"])) {
-		$allowedExts = array("gif", "jpeg", "jpg", "png");
-		$temp = explode(".", $_FILES["fileToUpload"]["name"]);
-		$extension = strtolower(end($temp));
+		if ($_FILES["fileToUpload"]["size"] != 0) {
+			$allowedExts = array("gif", "jpeg", "jpg", "png");
+			$temp = explode(".", $_FILES["fileToUpload"]["name"]);
+			$extension = strtolower(end($temp));
 
-		if (!in_array($extension, $allowedExts)) {
-			var_dump("Not allowed file");
-			die();
+			if (!in_array($extension, $allowedExts)) {
+				var_dump("Not allowed file");
+				die();
+			}
+
+			if ($_FILES["fileToUpload"]["error"] > 0) {
+				var_dump($_FILES["fileToUpload"]);
+				die();
+			} 
+
+			if ($_FILES["fileToUpload"]["size"] > 10000000) {
+				var_dump("Exceeded limit of image size");
+				die();
+			}
+				
+			$newfilename = round(microtime(true)) . '.' . end($temp);
+			move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], "upload/" . $newfilename);
 		}
-
-		if ($_FILES["fileToUpload"]["error"] > 0) {
-			var_dump($_FILES["fileToUpload"]);
-			die();
-		} 
-
-		if ($_FILES["fileToUpload"]["size"] > 10000000) {
-			var_dump("Exceeded limit of image size");
-			die();
-		}
-			
-		$newfilename = round(microtime(true)) . '.' . end($temp);
-		move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], "upload/" . $newfilename);
-
   	}
 ?>
