b7d86ef3a5eecb16d3467a69901c251f652ad043
RuslanGrigoryev <rus.grigoryev@yandex.ua>
2016-05-24 00:50:30 +0300

    Redesigned index

--- src/server/templates/footer/info.php
+++ src/server/templates/footer/info.php
@@ -9,6 +9,16 @@
 		<?php } ?>
 	</ul>
 
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 text-center logo">
+				<a href="http://luckfind-dev.me">
+					<img src="http://luckfind-dev.me/img/svg/logo.svg" alt="logo">
+				</a>		
+			</div>
+		</div>
+	</div>
+
 	<p class="info__copyright">
 		Copyright&nbsp;&nbsp;©&nbsp;&nbsp;2016&nbsp;&nbsp;<a href="http://<?php echo $GLOBALS['domain']; ?>">www.luckfind.me</a>
 	</p>
