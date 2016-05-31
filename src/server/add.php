6117f4f9c3ccbefeafea4032925a790812b4575b
RuslanGrigoryev <rus.grigoryev@yandex.ua>
2016-05-31 04:05:31 +0300

    Totally redesigned client forms

--- src/server/add.php
+++ src/server/add.php
@@ -22,8 +22,8 @@
 			<?php include_once $_SERVER['DOCUMENT_ROOT'].'/templates/steps/form.php'; ?>
 
 			<div class="row">
-				<div class="col-md-3"></div>
-				<div class="col-md-6">
+				<div class="col-md-2"></div>
+				<div class="col-md-8">
 					<?php if (!$GLOBALS['isAuthorised']) { ?>
 
 						<div class="row">
@@ -59,204 +59,262 @@
 						<?php
 							}
 						?>
-	
-						<div class="form-group">
+						
+						<div class="form-step">
 
-							<label class="col-sm-4 control-label" for="item">Предмет</label>
-
-							<div class="col-sm-8">
-							
-								<select class="form-control"
-										name="item"
-										id="item"
-										ng-model="sSubject"
-							 		    ng-change="iSubject = ''"
-										ng-options="code as code.name for code in codes track by code.name">
-										<option value="">Выберите из списка ниже:</option>
-								</select>
-								
-							</div>
-						</div>
-
-						<div class="form-group"
-							 ng-show="sSubject != '' && sSubject != null && sSubject.id != 1000">
-								
-							<label for="secret" class="col-sm-4 control-label">
-								Секретный код
-								<div class="g-info">
-									<div class="g-info__tooltip">{{sSubject.description}}</div>
-								</div>
-							</label>
-							
-							<div class="col-sm-8">
-							
-								<input type="text" 
-									   class="form-control"
-									   name="secret"
-									   id="secret"
-									   ng-model="secret"
-									   maxlength="100" 
-									   placeholder="Пример: {{sSubject.example}}" />
-
-							</div>	
-
-						</div>
-
-						<div class="form-group" ng-show="sSubject.id == 1000">
-							<label class="col-sm-4 control-label" for="user_item">Или введите своё</label>
-							<div class="col-sm-8">
-								<input 
-									type="text" 
-									name="user_item" 
-									id="user_item" 
-									class="form-control" 
-									placeholder="название..."
-									ng-model="iSubject"
-									ng-required="sSubject.id == 1000" />
-							</div>
-						</div>
-
-						<div class="form-group">
-							<label class="col-sm-4 control-label">Дата <?php if ($_GET['type'] == 'found') { ?> находки <?php } else { ?> пропажи <?php } ?></label>
-							<div class="col-sm-8 datepicker-wrapper">
-								<input 
-									type="text" 
-									id="datepicker" 
-									name="date" 
-									class="form-control"
-									data-default="12-05-2016" />
-							</div>
-						</div>
-
-						<div class="form-group">
-							<label class="col-sm-4 control-label" for="description">Дополнительная информация:</label>
-							<div class="col-sm-8">
-								<textarea 
-									ng-model="description"
-									ng-init="description = ''"
-									name="description" 
-									id="description" 
-									maxlength="300" 
-									class="form-control" 
-									placeholder="введите всю информацию"
-									required ></textarea>
-							</div>
-						</div>
-
-						<div class="form-group reward">
-							<label class="col-sm-4 control-label" for="reward">
-								<?php if ($_GET['type'] == 'found') { ?>
-									<span>
-											Вознаграждение
-									</span>
-								<?php } else { ?>
-									<span>
-											Сколько заплатите ?
-									</span>
-								<?php } ?>
-							</label>
-							<div class="col-sm-5" ng-init="reward = 0">
-								<div range-slider min="0" max="10000" model-max="reward" pin-handle="min"></div>
-							</div>
-							<div class="col-sm-3 text-center" style="margin: 9px 0 0 0;">
-								{{reward == 0 ? '+ в карму' : reward + ' грн'}}.
-							</div>
-						</div>
-
-						<div class="form-group">
-							<label class="col-sm-4 control-label" for="phone">Телефон:</label>
-							<div class="col-sm-8">
-								<input 
-									type="text" 
-									name="phone" 
-									id="phone" 
-									ng-model="phone"
-									ng-init="phone = ''"
-									class="form-control" 
-									placeholder="(___) ___-__-__"
-									ui-mask="(999) 999-99-99"
-									ui-mask-placeholder
-									ui-mask-placeholder-char="_" />
-							</div>
-						</div>
-
-						<div class="form-group">
-							<label class="col-sm-4 control-label" for="mail">E-mail:</label>
-							<div class="col-sm-8">
-								<input 
-									type="email" 
-									name="mail" 
-									id="mail" 
-									class="form-control" 
-									placeholder="электронная почта"
-									ng-model="mail"
-									ng-init="mail = ''"
-									required />
-							</div>
-						</div>
-
-						<div class="form-group">
-							<label class="col-sm-4 control-label" for="meta">Ключевые слова:</label>
-							<div class="col-sm-8">
-								<tags-input 
-									ng-model="tags"
-									max-tags="10"
-									placeholder="После ввода - Enter">
-								</tags-input>
-							</div>
-						</div>
-
-						<div class="form-group">
-							<label class="col-sm-4">
-								Отметьте на карте место <?php if ($_GET['type'] == 'found') { ?> находки <?php } else { ?> пропажи <?php } ?> : 
-							</label>
-
-							<div class="col-sm-3">
-								<a class="btn-geo btn-geo_green open-popup btn-map" data-type="map">Открыть карту</a>
+							<div class="form-step__badge" ng-init="fStep.toggle = true">
+								Шаг-1
 							</div>
 
-							<div class="col-sm-5">
-								<div class="row">
-									<div class="col-sm-8">
-										<input type="hidden" name="coordinates" id="coordinates" />
-										<p id="gmap-address"></p>
+							<div ng-class="{true : 'toggle toggle_up', false :'toggle toggle_down'}[fStep.toggle]" ng-click="fStep.toggle = !fStep.toggle">
+								{{fStep.toggle ? 'Свернуть' : 'Развернуть'}}
+							</div>
+
+							<div class="form-step__wrapper" ng-show="fStep.toggle">
+
+								<div class="form-group">
+
+									<div class="col-sm-7">
+									
+										<select class="form-control"
+												name="item"
+												id="item"
+												ng-model="sSubject"
+									 		    ng-change="iSubject = ''"
+												ng-options="code as code.name for code in codes track by code.name">
+												<option value="">Выберите из списка ниже:</option>
+										</select>
+										
 									</div>
 
-									<div class="col-sm-2">
-										<span id="clearAddress">&times;</span>
+									<label class="col-sm-5 control-label" for="item">
+										На первом шаге неоходимо указать найденный Вами предмет.
+									</label>
+								</div>
+
+								<div class="form-group"
+									 ng-show="sSubject != '' && sSubject != null && sSubject.id != 1000">
+									
+									<div class="col-sm-7">
+									
+										<input type="text" 
+											   class="form-control"
+											   name="secret"
+											   id="secret"
+											   ng-model="secret"
+											   maxlength="100" 
+											   placeholder="Пример: {{sSubject.example}}" />
+
+									</div>	
+
+									<label for="secret" class="col-sm-5 control-label">
+										Секретный код
+										<div class="g-info">
+											<div class="g-info__tooltip">{{sSubject.description}}</div>
+										</div>
+									</label>
+
+								</div>
+
+								<div class="form-group" ng-show="sSubject.id == 1000">
+									<div class="col-sm-7">
+										<input 
+											type="text" 
+											name="user_item" 
+											id="user_item" 
+											class="form-control" 
+											placeholder="название..."
+											ng-model="iSubject"
+											ng-required="sSubject.id == 1000" />
+									</div>
+
+									<label class="col-sm-5 control-label" for="user_item">Или введите своё</label>
+								</div>
+
+								<div class="form-group">
+									<div class="col-sm-7 datepicker-wrapper">
+										<input 
+											type="text" 
+											id="datepicker" 
+											name="date" 
+											class="form-control"
+											data-default="12-05-2016" />
+									</div>
+
+									<label class="col-sm-5 control-label">Дата <?php if ($_GET['type'] == 'found') { ?> находки <?php } else { ?> пропажи <?php } ?></label>
+								</div>
+
+								<div class="form-group">
+									<div class="col-sm-7">
+										<textarea 
+											ng-model="description"
+											ng-init="description = ''"
+											name="description" 
+											id="description" 
+											maxlength="300" 
+											class="form-control" 
+											placeholder="введите всю информацию"
+											required ></textarea>
+									</div>
+
+									<label class="col-sm-5 control-label" for="description">Дополнительная информация:</label>
+								</div>
+								
+							</div>
+
+						</div>
+
+						<div class="form-step">
+
+							<div class="form-step__badge" ng-init="fStep.toggle2 = true">
+								Шаг-2
+							</div>
+
+							<div ng-class="{true : 'toggle toggle_up', false :'toggle toggle_down'}[fStep.toggle2]" ng-click="fStep.toggle2 = !fStep.toggle2">
+								{{fStep.toggle2 ? 'Свернуть' : 'Развернуть'}}
+							</div>
+
+							<div class="form-step__wrapper" ng-show="fStep.toggle2">
+
+								<div class="form-group reward">
+									<div class="col-sm-5" ng-init="reward = 0">
+										<div range-slider min="0" max="10000" model-max="reward" pin-handle="min"></div>
+									</div>
+									<div class="col-sm-2 text-center" style="margin: 9px 0 0 0;">
+										{{reward == 0 ? '+ в карму' : reward + ' грн'}}.
+									</div>
+
+									<label class="col-sm-5 control-label" for="reward">
+										<?php if ($_GET['type'] == 'found') { ?>
+											<span>
+													Вознаграждение
+											</span>
+										<?php } else { ?>
+											<span>
+													Сколько заплатите ?
+											</span>
+										<?php } ?>
+									</label>
+								</div>
+
+								<div class="form-group">
+									<div class="col-sm-7">
+										<input 
+											type="text" 
+											name="phone" 
+											id="phone" 
+											ng-model="phone"
+											ng-init="phone = ''"
+											class="form-control" 
+											placeholder="(___) ___-__-__"
+											ui-mask="(999) 999-99-99"
+											ui-mask-placeholder
+											ui-mask-placeholder-char="_" />
+									</div>
+
+									<label class="col-sm-5 control-label" for="phone">Телефон:</label>
+								</div>
+
+								<div class="form-group">
+									<div class="col-sm-7">
+										<input 
+											type="email" 
+											name="mail" 
+											id="mail" 
+											class="form-control" 
+											placeholder="электронная почта"
+											ng-model="mail"
+											ng-init="mail = ''"
+											required />
+									</div>
+
+									<label class="col-sm-5 control-label" for="mail">E-mail:</label>
+								</div>
+
+							</div>
+
+						</div>
+
+						<div class="form-step">
+
+							<div class="form-step__badge" ng-init="fStep.toggle3 = true">
+								Шаг-3
+							</div>
+
+							<div ng-class="{true : 'toggle toggle_up', false :'toggle toggle_down'}[fStep.toggle3]" ng-click="fStep.toggle3 = !fStep.toggle3">
+								{{fStep.toggle3 ? 'Свернуть' : 'Развернуть'}}
+							</div>
+
+							<div class="form-step__wrapper" ng-show="fStep.toggle3">
+								<div class="form-group">
+									<div class="col-md-7">
+										<tags-input 
+											ng-model="tags"
+											max-tags="10"
+											placeholder="После ввода - Enter">
+										</tags-input>
+									</div>
+
+									<label class="col-md-5 control-label" for="meta">Ключевые слова:</label>
+								</div>
+
+								<div class="form-group">
+
+									<div class="col-md-3">
+										<a class="btn-geo btn-geo_green open-popup btn-map" data-type="map">Открыть карту</a>
+									</div>
+									<div class="col-md-9" style="padding-top: 5px;">
+										<div class="row">
+											<div class="col-md-10">
+												<input type="hidden" name="coordinates" id="coordinates" />
+												<p id="gmap-address"></p>
+											</div>
+
+											<div class="col-md-2">
+												<span id="clearAddress">&times;</span>
+											</div>
+										</div>
 									</div>
 								</div>
+
+								<div class="form-group">
+
+									<div class="col-md-7">
+										<label class="btn btn-default btn-upload">
+											<span>Загрузить фото</span>
+											<input type='file' id="addUploadFile" name="fileToUpload" />
+										</label>
+										
+										<div class="row">
+											<div class="col-md-12">
+												<img id="addUploadFilePreview" src="." alt="advert image" />
+											</div>
+										</div>
+									</div>
+
+									<label class="col-md-5 control-label">
+										Изображение <br />(не более 10 Мб)
+									</label>
+								</div>
+
+								<div class="form-group">
+									<div class="col-md-7">
+										<div class="g-recaptcha" 
+										     vc-recaptcha
+											 key="'6LetcxwTAAAAADqMNJtSZ1H_YEUZrmK-ygQofI4t'"
+											 theme="'light'"
+											 on-success="setResponse(response)"
+											 on-expire="cbExpiration()"></div>
+									</div>
+
+									<label for="mail_delivery" class="col-md-4 control-label">
+										<input type="checkbox" name="mail_delivery" id="mail_delivery" checked />
+										<span style="width:70%">Хотите получать первыми обьявления о похожих находках/пропажах :</span>
+									</label>
+								</div>
 							</div>
+
 						</div>
-
-						<div class="form-group">
-
-							<label class="col-sm-4">
-								Изображение <br />(не более 10 Мб)
-							</label>
-
-							<div class="col-sm-8">
-
-								<label class="btn btn-default btn-file">
-									<span>Загрузить</span>
-									<input type='file' id="addUploadFile" name="fileToUpload" />
-								</label>
-								<img id="addUploadFilePreview" src="." alt="advert image" />
-							
-							</div>
-						</div>
-
-						<label for="mail_delivery">
-							<input type="checkbox" name="mail_delivery" id="mail_delivery" checked />
-							<span style="width:70%">Хотите получать первыми обьявления о похожих находках/пропажах :</span>
-						</label>
-
-						<div class="g-recaptcha" 
-						     vc-recaptcha
-							 key="'6LetcxwTAAAAADqMNJtSZ1H_YEUZrmK-ygQofI4t'"
-							 theme="'light'"
-							 on-success="setResponse(response)"
-							 on-expire="cbExpiration()"></div>
 							
 						<?php if ($GLOBALS['isAuthorised']) { ?>
 
@@ -270,7 +328,7 @@
 						<?php } ?>
 					</form>
 				</div>
-				<div class="col-md-3"></div>
+				<div class="col-md-2"></div>
 			</div>
 
 		</div>
