872c10365e51f287e6b63cddbb3f35cd16a656d7
RuslanGrigoryev <rus.grigoryev@yandex.ua>
2016-06-04 23:55:00 +0300

    Added watermark to image after upload

--- src/server/app/save_image.php
+++ src/server/app/save_image.php
@@ -1,7 +1,7 @@
 <?php
 	if (isset($_FILES["fileToUpload"])) {
 		if ($_FILES["fileToUpload"]["size"] != 0) {
-			$allowedExts = array("gif", "jpeg", "jpg", "png");
+			$allowedExts = array("jpeg", "jpg", "png");
 			$temp = explode(".", $_FILES["fileToUpload"]["name"]);
 			$extension = strtolower(end($temp));
 
@@ -19,9 +19,29 @@
 				var_dump("Exceeded limit of image size");
 				die();
 			}
-				
+
 			$newfilename = round(microtime(true)) . '.' . end($temp);
+			$newfilename = 'wm-luckfind-'.$newfilename;
 			move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], "upload/" . $newfilename);
+				
+			$watermarkPath = 'http://'.$GLOBALS['domain'].'/img/watermark.png';
+			$stamp = imagecreatefrompng($watermarkPath);
+
+			if ($extension == "jpeg" || $extension == "jpg") {
+				$im = imagecreatefromjpeg("upload/" . $newfilename);
+			} else {
+				$im = imagecreatefrompng("upload/" . $newfilename);
+			}
+
+			$marge_right = 10;
+			$marge_bottom = 10;
+			$sx = imagesx($stamp);
+			$sy = imagesy($stamp);
+
+			imagecopy($im, $stamp, imagesx($im) - $sx - $marge_right, imagesy($im) - $sy - $marge_bottom, 0, 0, imagesx($stamp), imagesy($stamp));
+
+			imagepng($im, "upload/" . $newfilename);
+			imagedestroy($im);
 		}
   	}
 ?>
